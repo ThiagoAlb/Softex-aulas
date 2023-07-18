c9f39da807c2d0aac14fb4a9982a7c567640f25d
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-07-18 19:01:17 +0000

    Primeiro commit

---
+++ aula01.js
@@ -0,0 +1,12 @@
+console.log("Ola mundo");
+
+
+console.log("======== MENU ========");
+console.log("1 - Listar clientes");
+console.log("2 - Cadastrar clientes");
+console.log("3 - Editar clientes");
+console.log("4 - Remover cliente");
+console.log("0 - Sair do sistema");
+console.log("========================");
+console.log("OPÇÃO: __");
+console.log("========================");
