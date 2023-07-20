d4ab094355a7334047edd52445c1967b975b3c9c
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-07-20 20:02:28 +0000

    Terceiro exercicio da aula 02

---
+++ aula02-2.js
@@ -0,0 +1,41 @@
+//Classifique um número em três categorias: negativo, zero ou positivo
+let num = 5;
+
+if(num < 0) {
+    console.log("Negativo")
+} else if (num = 0) {
+    console.log("Igual a zero")
+} else {
+    console.log("Positivo")
+}
+
+//02)
+if (num % 2 === 0) {
+    console.log("Divisivel por 2")
+} else if (num % 3 === 0) {
+    console.log("Divisivel por 3")
+} else if (num % 5 === 0){
+    console.log("Divisivel por 3")
+}
+
+//03)
+if(num === true || num === false) {
+    console.log("É booleano")
+} else if (num === number) {
+    console.log("É número")
+} else if (num === string) {
+    console.log("É string")
+} else if (num === undefined) {
+    console.log("É indefinido")
+}
+
+//04)
+if(num < 0) {
+    
+} else if (num = 0) {
+    console.log("Número igual a zero")
+} else if(num > 0) {
+    console.log("Número positivo")
+} else if (num % 2 === 0) {
+    console.log("Par")
+}
