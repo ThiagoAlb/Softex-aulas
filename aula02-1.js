207f77cc7f758840667b0198164aa020bb1d4365
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-07-20 19:48:48 +0000

    Segundo exercicio da aula 02

---
+++ aula02-1.js
@@ -0,0 +1,38 @@
+//Verifique se um número é positivo.
+const num = 8;
+if(num >= 0) {
+    console.log("Esse número é positivo.")
+} else {
+    console.log("Esse número é negativo.")
+}
+
+//Verifique se um número é par
+if (num % 2 == 0) {
+    console.log("Esse número é par")
+} else {
+    console.log("Esse número é impar.")
+}
+
+//Verifique se uma string é vazia
+let string = "";
+if(string === "") {
+    console.log("Essa string está vazia")
+} else {
+    console.log(string)
+}
+
+//Verifique se um número é maior que 10.
+if (num > 10) {
+    console.log("Esse número é maior que 10")
+} else {
+    console.log(num * 2)
+}
+
+//Verifique se uma variável é do tipo booleano.
+let booleano = "É booleano?";
+
+if (booleano === true || booleano === false) {
+    console.log("Variável booleana.")
+} else {
+    console.log(booleano = true)
+}
