f977f7ce09325c407753eb19f3a243c708a9fd73
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-07-25 18:00:25 +0000

    Correções

--- aula02-2.js
+++ aula02-2.js
@@ -1,41 +1,56 @@
 //Classifique um número em três categorias: negativo, zero ou positivo
-let num = 5;
+let num = true;
+let tipoNum = typeof(num);
 
 if(num < 0) {
-    console.log("Negativo")
-} else if (num = 0) {
-    console.log("Igual a zero")
-} else {
-    console.log("Positivo")
+    console.log("Negativo");
+} else if (num === 0) {
+    console.log("Igual a zero");
+} else if (num > 0){
+    console.log("Positivo");
 }
 
-//02)
+//Verifique se um número é divisível por 2, 3 ou 5.
+if (num % 2 === 0) {
+    console.log("Divisível por 2")
+} else if (num % 3 === 0) {
+    console.log("Divisível por 3")
+} else if (num % 5 === 0){
+    console.log("Divisível por 3")
+}
+
+//Classifique uma variável em quatro categorias: booleana, numérica, string ou indefinida.
+if(tipoNum === true || tipoNum === false) {
+    console.log("É booleano")
+} else if (tipoNum === typeof(1)) {
+    console.log("É número")
+} else if (tipoNum === typeof("x")) {
+    console.log("É string")
+} else if (tipoNum === undefined) {
+    console.log("É indefinido")
+}
+
+//Classifique um número em cinco categorias: negativo, zero, positivo, par ou impar
+if(num < 0) {
+    console.log("Número menor que zero")
+} else if (num === 0) {
+    console.log("Número igual a zero")
+} else if(num > 0) {
+    console.log("Número positivo")
+} else if (num % 2 === 0) {
+    console.log("Par")
+} else if (num % 2 !== 0 && tipoNum === typeof(5)) {
+    console.log("Número ímpar.")
+}
+
+
+//Verifique se um número é divisível por 2, 3, 5 ou 7
 if (num % 2 === 0) {
     console.log("Divisivel por 2")
 } else if (num % 3 === 0) {
     console.log("Divisivel por 3")
 } else if (num % 5 === 0){
     console.log("Divisivel por 3")
-}
-
-//03)
-if(num === true || num === false) {
-    console.log("É booleano")
-} else if (num === number) {
-    console.log("É número")
-} else if (num === string) {
-    console.log("É string")
-} else if (num === undefined) {
-    console.log("É indefinido")
-}
-
-//04)
-if(num < 0) {
-    
-} else if (num = 0) {
-    console.log("Número igual a zero")
-} else if(num > 0) {
-    console.log("Número positivo")
-} else if (num % 2 === 0) {
-    console.log("Par")
+} else if (num % 7 === 0) {
+    console.log("Divisivel por 7")
 }
