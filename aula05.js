c110de3c91e86e9960438cf0236de5114ccdc3f4
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-08-07 20:06:56 +0000

    Resolucao de exercicios

--- aula05.js
+++ aula05.js
@@ -1,8 +1,9 @@
 const pergunta = require("readline-sync");
 
 //Imprima os números de 1 a 10.
-let i = 1;
 /*
+let i = 1;
+
 while(i <= 10) {
     console.log(i);
     i++;
@@ -33,9 +34,9 @@ console.log(`A quantidade de números pares é igual a: ${quantPares}`);
 */
 
 //Multiplique um número por 2 até que o resultado seja maior que 1000.
-
-let numero = pergunta.questionInt("Informe um número inteiro: ");
 /*
+let numero = pergunta.questionInt("Informe um número inteiro: ");
+
 while (numero <= 1000) {
     numero = numero * 2;
 }
@@ -65,9 +66,14 @@ if (ePrimo) {
 /*
 let senha = pergunta.questionInt("Informe sua senha: ");
 
-while (senha !== ) {
-    
+let confirmarSenha = pergunta.questionInt("Confirme sua senha: ");
+
+while (confirmarSenha !== senha) {
+    console.log("SENHA INCORRETA! Tente novamente.")
+    confirmarSenha = pergunta.questionInt("Confirme sua senha: ");
 }
+
+console.log("Senha Correta! Acesso permitido.");
 */
 
 //Imprima os múltiplos de 3 de 1 a 30
@@ -83,9 +89,37 @@ while (m <= 30) {
 */
 
 //Calcule a média de uma lista de números.
+/*
+const lista = [];
+let n = 1;
+let quantNumeros = pergunta.questionInt("Quantos números você quer tirar a média? ");
+while (n <= quantNumeros) {
+    let numero = pergunta.questionInt("Digite um número: ");
+    lista.push(numero)
+    n++
+}
 
+let soma = 0;
+let t = 0;
+while (t < lista.length) {
+    soma += lista[t];
+    t++;
+}
+
+const media = soma / quantNumeros;
+console.log(`A média dos números é igual a ${media}`);
+*/
 
 //Calcule o fatorial de um número.
+
+let fatNumero = pergunta.questionInt('Qual numero deseja fatorar? ');
+let somaFatNumero = 0;
+while (fatNumero >= 2) {
+    let resultado = fatNumero * (fatNumero - 1);
+    somaFatNumero = somaFatNumero + resultado;
+    fatNumero--;
+    console.log(somaFatNumero);
+}
 
 
 //Imprima os números de 10a 1 em ordem decrescente.
